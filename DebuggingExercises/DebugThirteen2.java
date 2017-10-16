af99b0ba69dfa75210ff3edb9708ad312f7ed372
Hamish <hamishorr@my.jcu.edu.au>
2017-10-16 17:09:08 +1000

    start prac 8 ch13

--- DebuggingExercises/DebugThirteen2.java
+++ DebuggingExercises/DebugThirteen2.java
@@ -1,6 +1,8 @@
 // Program reads in a file of phone numbers without area codes
 // inserts "(312) " in front of each phone number
 // and produces an output file with the new complete phone numbers
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
 import java.nio.file.*;
 import java.io.*;
 import java.nio.channels.FileChannel;
@@ -10,33 +12,39 @@ public class DebugThirteen2
 {
    public static void main(String[] args)
    {
+      FileSystem fs = FileSystems.getDefault();
       Path fileIn =
-         Paths.get(C:\\Java\\Chapter.13\\DebugData3.txt);
+         fs.getPath("C:\\Java\\Chapter.13\\DebugData3.txt");
       Path fileOut =
-         Paths.get(C:\\Java\\Chapter.13\\DebugData3New.txt);
+         fs.getPath("C:\\Java\\Chapter.13\\DebugData3New.txt");
       String areaCode = "(312) ";
-      String phone;
+      String phone = null;
       InputStream input = null;
-      OutputStream output = null
+
+
       try
       {
          input = Files.newInputStream(fileIn);
-         BufferedReader reader = new BufferedReader
-            (new InputStreamReader(input);
-         output = Files.newOutputStream(fileOut);
-         phone = input.readLine();
-         while(phone == null)
+         BufferedReader reader = new BufferedReader(new InputStreamReader((input)));
+
+         phone = reader.readLine();
+         while (phone != null)
          {
             phone = areaCode + phone + System.getProperty("line.separator");
-            byte[] phoneBytes = phone.getBytes();
-            output.writeln(phoneBytes);
-            phone = reader.readline();
+            byte[] data = phone.getBytes();
+            OutputStream output = null;
+            output = Files.newOutputStream(fileOut);
+            output.write(data);
+            output.flush();
+            output.close();
+
+            phone = reader.readLine();
          }
          input.close();
        }
-       catch (IOException e)
+       catch (Exception e)
        {
-          System.out.println(e);
+          System.out.println("Message: " + e);
        }
    }
 }
